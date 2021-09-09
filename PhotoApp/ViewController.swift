50efb010e3c8a981a9ed662feb014e4c9d86b423
Afir Thes <afirthes@gmail.com>
2021-09-09 21:32:01 +0300

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Afir Thes on 09.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
